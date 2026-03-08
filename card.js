765a31065ec469ae9b2251f6a6f223ca01ccd8bd
Moeen-Islam <moeenislam809@gmail.com>
2026-03-08 15:39:33 +0600

    fetch the data and set variable

---
+++ card.js
@@ -0,0 +1,42 @@
+
+let currentFilter = "all";
+const API_URLAll= "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+let issues = [];
+
+// variable declear
+const allBtn = document.getElementById("allBtn");
+const openBtn = document.getElementById("openBtn");
+const closedBtn = document.getElementById("closedBtn");
+const totalIssues = document.getElementById("totalIssues");
+const issuesContainer = document.getElementById("issuesContainer");
+const searchInput = document.getElementById("searchInput");
+
+
+// active btn Function
+function setActiveButton(type) {
+      allBtn.className = "w-[90px] sm:w-[120px] shrink-0 btn bg-white text-[#64748B]";
+      openBtn.className = "w-[90px] sm:w-[120px] shrink-0 btn bg-white text-[#64748B]";
+      closedBtn.className = "w-[90px] sm:w-[120px] shrink-0 btn bg-white text-[#64748B]";
+
+      if (type === "all") {
+        allBtn.className = "w-[90px] sm:w-[120px] shrink-0 btn btn-primary text-white";
+      } else if (type === "open") {
+        openBtn.className = "w-[90px] sm:w-[120px] shrink-0 btn btn-primary text-white";
+      } else if (type === "closed") {
+        closedBtn.className = "w-[90px] sm:w-[120px] shrink-0 btn btn-primary text-white";
+      }
+    }
+
+
+
+// Fetch the data
+async function loadIssues() {
+        const response = await fetch(API_URLAll);
+        const data = await response.json();
+
+        issues = Array.isArray(data) ? data : (data.data || data.issues || []);
+        updateTotalCount();
+        renderIssues();
+}
+setActiveButton("all");
+loadIssues();
--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-let currentFilter = "all";
-const API_URLAll= "https://phi-lab-server.vercel.app/api/v1/lab/issues";
-let issues = [];
-
-// variable declear
-const allBtn = document.getElementById("allBtn");
-const openBtn = document.getElementById("openBtn");
-const closedBtn = document.getElementById("closedBtn");
-const totalIssues = document.getElementById("totalIssues");
-const issuesContainer = document.getElementById("issuesContainer");
-const searchInput = document.getElementById("searchInput");
-
-
-
-
 // Login Page 
 document.getElementById("signinBtn").addEventListener("click", (event) => {
     event.preventDefault();
@@ -33,23 +18,5 @@ document.getElementById("signinBtn").addEventListener("click", (event) => {
     }
 })
 // Login Page End
-
-// active btn Function
-function setActiveButton(type) {
-      allBtn.className = "w-[90px] sm:w-[120px] shrink-0 btn bg-white text-[#64748B]";
-      openBtn.className = "w-[90px] sm:w-[120px] shrink-0 btn bg-white text-[#64748B]";
-      closedBtn.className = "w-[90px] sm:w-[120px] shrink-0 btn bg-white text-[#64748B]";
-
-      if (type === "all") {
-        allBtn.className = "w-[90px] sm:w-[120px] shrink-0 btn btn-primary text-white";
-      } else if (type === "open") {
-        openBtn.className = "w-[90px] sm:w-[120px] shrink-0 btn btn-primary text-white";
-      } else if (type === "closed") {
-        closedBtn.className = "w-[90px] sm:w-[120px] shrink-0 btn btn-primary text-white";
-      }
-    }
-
-    
-
 
 
